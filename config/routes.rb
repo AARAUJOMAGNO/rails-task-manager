87f6eeb1f8cae0a1a2653d5a0be05a1c81341d7d
Adriano Araujo <aaraujo2000@gmail.com>
2019-05-21 17:25:08 -0300

    Rail Task Manager working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ (list)
+
+  get 'tasks', to: "tasks#index"
+
+  # # CREATE
+
+  get 'tasks/new', to: "tasks#new"
+  post 'tasks', to: "tasks#create"
+
+  # # Show one task
+
+  get 'tasks/:id', to: "tasks#show", as: :task
+
+  # # UPDATE
+  # # Form to edit the restaurant data
+  get 'tasks/:id/edit', to: "tasks#edit", as: :task_edit
+  patch 'tasks/:id', to: "tasks#update"
+
+  # # DESTROY
+  delete 'tasks/:id', to: "tasks#destroy"
+
+  # /restaurants/3
+  # params[:id]
 end
